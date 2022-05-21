e723d04c3caa95432f44398de310dbb365d68919
ttronge <tomastronge@gmail.com>
2022-05-21 16:47:32 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [gifs, setGifs] = useState(['https://media0.giphy.com/media/EPcvhM28ER9XW/200w.webp?cid=ecf05e47zuapvzpgrsn36xuv2x1j9aenb71qumtyqxgoy3l1&rid=200w.webp&ct=g']) 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        <img src={gifs} alt="" />
+        gifs
+      </section>
     </div>
   );
 }
 
 export default App;
+
+// import { useState,useEffect } from 'react';
+
+// function App() {
+//   const [gif,setGif] = useState([])
+//   useEffect(()=>{
+//     fetch('https://api.giphy.com/v1/gifs/search?api_key=jGbjzvogAKINV2OSxJGPMjWGDC4DlIfj&q=panda&limit=10&offset=0&rating=g&lang=en')
+//     .then((data)=>{
+//       data.json()
+//       .then(({data})=>setGif(data));
+//       })
+//   },[])
+//     console.log(gif);
+
+//   return (
+//     <div className="App">
+
+//     </div>
+//   );
+// }
